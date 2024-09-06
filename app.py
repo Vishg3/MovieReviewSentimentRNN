d6487a004b814f1113bd10d0afd85f1336cb081e
Vishnu Gupta <132547399+Vishg3@users.noreply.github.com>
2024-09-06 11:16:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
+import streamlit as st
+
+model=load_model('simple_rnn_imdb.h5')
+word_index=imdb.get_word_index()
+
+def preprocess_text(text):
+    words=text.lower().split()
+    encoded_review=[word_index.get(word, 2) + 3 for word in words]
+    padded_review=sequence.pad_sequences([encoded_review], maxlen=500)
+    return padded_review
+
+st.title('IMDB Movie Review Sentiment Analysis')
+st.write('Enter a movie review to classify sentiment as positive or negative.')
+
+user_input=st.text_area('Movie Review')
+
+if st.button('Classify'):
+    preprocessed_input=preprocess_text(user_input)
+    prediction=model.predict(preprocessed_input)
+    sentiment='Positive' if (prediction>0.5) else 'Negative'
+    st.write(f'Sentiment: {sentiment}')
+    st.write(f'Prediction Score: {prediction}')
+else:
+    st.write('Please enter a movie review.')
